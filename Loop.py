8ace345c6fbaf4e09fc1c8231b239c738c8b9138
uchikezie <71209140+uchikezie@users.noreply.github.com>
2022-05-08 22:49:14 -0700

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,37 @@
+from ast import Break
+
+print('Demonstrating loops')
+print('Prints a range of numbers from 3 to 7')
+for i in range(3, 8):
+     print(i)
+
+print('Prints a range of numbers from -5 to 9 in +1 increment')
+# i = -5
+# while i<10:
+#     print(i)
+#     i=i+2
+
+print('Printing from 1 to 9 ans skipping 7')
+# for i in range(1, 10):
+#     if i == 7:
+#         continue
+#     print(i)
+
+print('Printing from 1 to 11 and skipping 3 & 5')
+# for i in range(1, 12):
+#     if i in (3,5):
+#         continue
+#     print(i)
+
+print('Printing 2 to 8 and skipping 4')
+# for i in range(2, 9):
+#     if i == 4:
+#         continue
+#     print (i)
+
+print('Printing from 2 to 9 but breaks at 4')
+# for i in range(2, 9):
+#     if i == 4:
+#         break
+#     print (i)
+
