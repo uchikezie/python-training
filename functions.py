71fde652b3ddd5bd3a5c7e0b1908dbc857b18c73
uchikezie <71209140+uchikezie@users.noreply.github.com>
2022-06-25 23:33:27 -0700

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,13 @@
+#Global Variable
+def spam():
+    eggs = 99
+    print(eggs) 
+spam()
 
+
+#calling name function
+def hello(name):
+    print('hello ' + name)
+
+hello('Ugonna')
+hello('Chikezie')
