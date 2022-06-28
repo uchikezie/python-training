0daec95acbb43e1874ef2b69072f28dbb04ec610
uchikezie <71209140+uchikezie@users.noreply.github.com>
2022-06-27 21:48:37 -0700

    Update Functions1.py

--- Functions1.py
+++ Functions1.py
@@ -1,1 +1,13 @@
+#This program defines the function (Hello) and calls the function to print (Howdy)
+def Hello():
+    print('Howdy!')
+    print('Howdy!!')
+    print('Howdy!!!')
+Hello()
+Hello()
 
+#Adding a name variable to the Hello program and calling the Hello function
+def Hello(name):
+    print('Your name is ' + name)
+Hello('Ugonna')
+Hello ('Chikezie')
